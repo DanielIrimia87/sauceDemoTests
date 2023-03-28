783e1c75e03513a9439f985bd551b97f12f2e684
DanielIrimia87 <87778687+DanielIrimia87@users.noreply.github.com>
2023-03-28 18:12:28 +0200

    added to the SauceDeme.java file more WebElements

--- src/main/java/pageObjects/SauceDemo.java
+++ src/main/java/pageObjects/SauceDemo.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class SauceDemo {
     public static final String APP_URL = "https://www.saucedemo.com";
 
@@ -25,6 +27,27 @@ public class SauceDemo {
     @FindBy(xpath = ERROR_BUTTON)
     public WebElement errorButton;
     // end of Login page elements declaration and initialization
+
+    // Inventory page elements declaration and initialization
+    public static final String INVENTORY_ITEMS = "div[class='inventory_item']";
+    public static final String INVENTORY_ITEM_NAME = "div[class='inventory_item_name']";
+    public static final String INVENTORY_ITEM_DESCRIPTION = "div[class='inventory_item_desc']";
+    public static final String INVENTORY_ITEM_PRICE = "div[class='inventory_item_price']";
+    public static final String INVENTORY_ITEM_ADD_TO_CART = "button[class='btn btn_primary btn_small btn_inventory']";
+    public static final String INVENTORY_ITEM_REMOVE = "button[class='btn btn_secondary btn_small btn_inventory']";
+
+    @FindBy(css = INVENTORY_ITEMS)
+    public List<WebElement> inventoryItems;
+    @FindBy(css = INVENTORY_ITEM_NAME)
+    public WebElement inventoryItemName;
+    @FindBy(css = INVENTORY_ITEM_DESCRIPTION)
+    public WebElement inventoryItemDescription;
+    @FindBy(css = INVENTORY_ITEM_PRICE)
+    public WebElement inventoryItemPrice;
+    @FindBy(css = INVENTORY_ITEM_ADD_TO_CART)
+    public WebElement inventoryItemAddToCart;
+    @FindBy(css = INVENTORY_ITEM_REMOVE)
+    public WebElement inventoryItemRemove;
 
     public SauceDemo(WebDriver driver) {
          PageFactory.initElements(driver, this);
