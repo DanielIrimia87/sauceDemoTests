9dadda997bcaec948503ba7c9b34216f959ee1fd
DanielIrimia87 <87778687+DanielIrimia87@users.noreply.github.com>
2023-04-05 11:54:16 +0200

    modified: OrderTests

--- src/test/java/saucedemo/com/OrderTests.java
+++ src/test/java/saucedemo/com/OrderTests.java
@@ -8,6 +8,9 @@ import org.testng.annotations.Test;
 
 import java.util.List;
 
+import static org.testng.Assert.assertTrue;
+
+
 public class OrderTests extends BaseTest {
     @Test(description = "Verify that adding elements to the cart works as expected")
     public void addItemsToCart() {
@@ -20,23 +23,48 @@ public class OrderTests extends BaseTest {
         WebElement firstItemAddToCart = page.inventoryItemAddToCart;
         Assert.assertEquals(firstItemAddToCart.getText().toLowerCase(), "add to cart",
                 "The first item in the inventory should have an 'Add to cart' button");
-        Assert.assertFalse(isElementPresent(By.xpath(page.SHOPPING_CART_BADGE)),
+        Assert.assertFalse(isElementPresent(By.cssSelector(page.SHOPPING_CART_BADGE)),
                 "The shopping cart badge should not be present before adding items to the cart");
         firstItemAddToCart.click();
         Assert.assertEquals(page.inventoryItemRemove.getText().toLowerCase(), "remove",
                 "The first item in the inventory should have a 'Remove' button");
-        Assert.assertTrue(isElementPresent(By.cssSelector(page.SHOPPING_CART_BADGE)),
+        assertTrue(isElementPresent(By.cssSelector(page.SHOPPING_CART_BADGE)),
                 "The shopping cart badge is not present after adding items to the cart");
         Assert.assertEquals(page.shoppingCartBadge.getText().toLowerCase(), "1",
                 "Number of items in the shopping cart is incorrect");
         shoppingCart.click();
-        Assert.assertTrue(driver.getCurrentUrl().contains("cart.html"),
+        assertTrue(driver.getCurrentUrl().contains("cart.html"),
                 "User is not redirected to the shopping cart page after clicking on the shopping cart link");
+        List<WebElement> cartItems = page.cartItems;
+        String cartItemName = page.inventoryItemName.getText();
+        Assert.assertEquals(cartItemName, firstItemTitle,
+                "Wrong item added to the shopping cart");
+        String cartItemPrice = page.inventoryItemPrice.getText();
+        Assert.assertEquals(cartItemPrice, firstItemPrice,
+                "Wrong item added to the shopping cart");
+
+        assertTrue(isElementPresent(cartItems.get(0), By.cssSelector(".btn.btn_secondary.btn_small.cart_button")),
+                "Remove button on card item is not present ");
+        assertTrue(isElementPresent(By.id("continue-shopping")),
+                "Continue shopping is not present ");
+        assertTrue(isElementPresent(By.id("checkout")),
+                "Checkout is not present ");
+
+        System.out.println("The first item in the inventory was added to the shopping cart successfully");
     }
 
     private boolean isElementPresent(By locator) {
         try {
             driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isElementPresent(WebElement element, By locator) {
+        try {
+            element.findElement(locator);
             return true;
         } catch (NoSuchElementException e) {
             return false;
