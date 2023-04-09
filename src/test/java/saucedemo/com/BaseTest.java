1461b08584bfeb36bf9b9a913165feb0f6083860
DanielIrimia87 <87778687+DanielIrimia87@users.noreply.github.com>
2023-04-09 10:37:46 +0200

    modified :BaseTest

--- src/test/java/saucedemo/com/BaseTest.java
+++ src/test/java/saucedemo/com/BaseTest.java
@@ -1,7 +1,10 @@
 package saucedemo.com;
 
 import Utilities.Utils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pageObjects.SauceDemo;
@@ -10,20 +13,48 @@ public abstract class BaseTest extends Utils {
     protected WebDriver driver = null;
     SauceDemo page = null;
 
-    @BeforeMethod
-    public void setup() {
+    @BeforeMethod(onlyForGroups = {"with-login"})
+    public void setupWithLogin() {
         driver = getChromeDriver();
         driver.get(SauceDemo.APP_URL);
         page = new SauceDemo(driver);
-        page.inputUsername.sendKeys("standard_user");
-        page.inputPassword.sendKeys("secret_sauce");
-        page.loginButton.click();
+        loginSteps("standard_user", "secret_sauce");
+    }
+    @BeforeMethod(onlyForGroups = {"without-login"})
+    public void setupWithoutLogin() {
+        driver = getChromeDriver();
+        driver.get(SauceDemo.APP_URL);
+        page = new SauceDemo(driver);
     }
 
     @AfterMethod(alwaysRun = true)
     public void quit() {
         if (driver != null) {
             driver.quit();
+        }
+    }
+
+    protected void loginSteps(String username, String password) {
+        page.inputUsername.sendKeys(username);
+        page.inputPassword.sendKeys(password);
+        page.loginButton.click();
+    }
+
+    protected boolean isElementPresent(By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    protected boolean isElementPresent(WebElement element) {
+        try {
+            element.isDisplayed();
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
         }
     }
 }
